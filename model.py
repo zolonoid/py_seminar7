4249f8f304194b95e69af63a850d37045640575d
John Doe <jd@mail.web>
2023-01-02 21:15:03 +0300

    completed

--- model.py
+++ model.py
@@ -20,11 +20,11 @@ class Contact:
         return self._phone
     
     def is_valid(self)->bool:
-        if re.fullmatch(r'^\w+$',self.surname) is None:
+        if re.fullmatch(r'^\w+$',self._surname) is None:
             return False
-        if re.fullmatch(r'^\w+$',self.name) is None:
+        if re.fullmatch(r'^\w+$',self._name) is None:
             return False
-        if re.fullmatch(r'^[\d()+-]+$',self.phone) is None:
+        if re.fullmatch(r'^[\d()+-]+$',self._phone) is None:
             return False
         return True
 
@@ -36,6 +36,7 @@ class PhoneBook:
 
     def add_contact(self,contact: Contact,save=True)->None:
         if not contact.is_valid():
+            print(f"{contact.surname()} {contact.name()} {contact.phone()}")
             raise ValueError
         self._book.append(contact)
         if save:
@@ -57,28 +58,28 @@ class PhoneBook:
         return [i for i in self._book if i.phone()==phone]
 
     def import_from_txt(self,file: Path) -> None:
-        for i in file.read_text().split(''):
+        for i in file.read_text('utf-8').split('*\n'):
             contact=[c for c in i.split('\n')]
             self.add_contact(Contact(contact[0],contact[1],contact[2]),False)
         self._save()
 
     def import_from_csv(self,file: Path) -> None:
-        for i in file.read_text().split('\n'):
+        for i in file.read_text('utf-8').split('\n'):
             contact=[c for c in i.split(';')]
             self.add_contact(Contact(contact[0],contact[1],contact[2]),False)
         self._save()
 
     def export_to_txt(self,file: Path) -> None:
-        pass
+        file.write_text('\n*\n'.join([f"{i.surname()}\n{i.name()}\n{i.phone()}" for i in self._book]),'utf-8')
 
     def export_to_csv(self,file: Path) -> None:
-        pass
+        file.write_text('\n'.join([f"{i.surname()};{i.name()};{i.phone()}" for i in self._book]),'utf-8')
 
     def _save(self) -> None:
-        Path("phone_book.csv").write_text('\n'.join([f"{i.surname()};{i.name()};{i.phone()}" for i in self._book]))
+        Path("phone_book.csv").write_text('\n'.join([f"{i.surname()};{i.name()};{i.phone()}" for i in self._book]),'utf-8')
 
     def _load(self) -> None:
-        for i in Path("phone_book.csv").read_text().split('\n'):
+        for i in Path("phone_book.csv").read_text('utf-8').split('\n'):
             contact=[c for c in i.split(';')]
             if len(contact) != 3: continue
             self.add_contact(Contact(contact[0],contact[1],contact[2]),False)
--- view.py
+++ view.py
@@ -10,41 +10,41 @@ class PhoneBookShell(cmd.Cmd):
         self._controller=controller
 
     def do_add(self,arg):
-        'Добавить контакт:  ADD Фамилия Имя Телефон'
+        'Добавить контакт:  add Фамилия Имя Телефон'
         self._controller.add_contact(arg)
 
     def do_all(self,arg):
-        'Показать все контакты:  ALL'
+        'Показать все контакты:  all'
         print('\n'.join(self._controller.all_contacts()))
 
     def do_fullname(self,arg):
-        'Показать контакты с указанными фамилией и именем:  FULLNAME Фамилия Имя'
+        'Показать контакты с указанными фамилией и именем:  fullname Фамилия Имя'
         print('\n'.join(self._controller.contacts_by_fullname(arg)))
     
     def do_surname(self,arg):
-        'Показать контакты с указанной фамилией:  SURNAME Фамилия'
+        'Показать контакты с указанной фамилией:  surname Фамилия'
         print('\n'.join(self._controller.contacts_by_surname(arg)))
 
     def do_name(self,arg):
-        'Показать контакты с указанным именем:  NAME Имя'
+        'Показать контакты с указанным именем:  name Имя'
         print('\n'.join(self._controller.contacts_by_name(arg)))
 
     def do_phone(self,arg):
-        'Показать контакты с указанным телефоном:  PHONE Телефон'
+        'Показать контакты с указанным телефоном:  phone Телефон'
         print('\n'.join(self._controller.contacts_by_phone(arg)))
 
     def do_fromtxt(self,arg):
-        'Импортировать контакты из файла .txt:  FROMTXT Файл'
+        'Импортировать контакты из файла .txt:  fromtxt Файл'
         self._controller.import_from_txt(arg)
 
     def do_fromcsv(self,arg):
-        'Импортировать контакты из файла .csv:  FROMCSV Файл'
+        'Импортировать контакты из файла .csv:  fromcsv Файл'
         self._controller.import_from_csv(arg)
 
     def do_totxt(self,arg):
-        'Экспортировать контакты в файл .txt:  TOTXT Файл'
+        'Экспортировать контакты в файл .txt:  totxt Файл'
         self._controller.export_to_txt(arg)
 
     def do_tocsv(self,arg):
-        'Экспортировать контакты в файл .csv:  TOCSV Файл'
+        'Экспортировать контакты в файл .csv:  tocsv Файл'
         self._controller.export_to_csv(arg)
